de3ab7f131f3f0a80f4c8f62e28ebfce6adb80fa
Martin Borst <martinborst1@web.de>
2024-01-03 12:56:50 +0100

    feat: add migration for renamed, added and removed properties of json object

--- JSONMigrationDemo/EmbeddedExampleType.cs
+++ JSONMigrationDemo/EmbeddedExampleType.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = null!;
+        public string NameRenamed { get; set; } = null!;
 
-        public string Description { get; set; } = null!;
+        public double NewProperty { get; set; }
     }
 }
---
+++ JSONMigrationDemo/Migrations/20240103115329_Renamed Name property, added NewProperty, removed Description.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JSONMigrationDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedNamepropertyaddedNewPropertyremovedDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // expectation:
+            // automatically generated migration detects changes of type saved as JSON column
+            // what I found for manual appraoch: https://stackoverflow.com/questions/53433285/mysql-update-or-rename-a-key-in-json
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // same for reverting a migration
+        }
+    }
+}
